9ca2f0a22e48f752ab5210b7ff04391853edab79
Daniil Zhdanov <yeezysmem@gmail.com>
2021-07-02 15:09:20 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import styles from './App.module.scss';
+// import Header from './materials/Header/Header'
+import Devices from './materials/Devices/Devices'
 
 function App() {
   return (
     <div className={styles.App}>
-      
+      {/* <Header /> */}
+      <Devices />
     </div>
   );
 }
