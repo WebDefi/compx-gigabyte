88bedfdab2f00bddbfcffe643c3eea040ef67ff9
Daniil Zhdanov <yeezysmem@gmail.com>
2021-07-02 15:08:52 +0300

    Create DevicesComponent.jsx

---
+++ src/materials/Devices/components/DevicesComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import styles from './DevicesComponent.module.scss'
+
+const DevicesComponent = ({ deviceImg,deviceBg, title }) => {
+  return(
+    <div className={styles.DevicesComponent} style={{position: 'relative'}}>
+    	<img height="auto" width="65%" src={deviceImg} style={{display:"inline-block",bottom:0,backgroundImage:`url(${deviceBg})`,backgroundRepeat:"no-repeat", backgroundPosition:"100% 12%"}}></img>
+        
+        <h5 style={{textAlign:"center"}}>{title}</h5>
+    </div>
+  )
+}
+
+export default DevicesComponent;
