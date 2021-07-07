4cbe911c8fd2f3b4097d737b6882aa7321f3e585
Daniil Zhdanov <yeezysmem@gmail.com>
2021-07-07 20:18:00 +0300

    +

---
+++ src/materials/Footer/components/FooterCol.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Row, Col, Container } from "reactstrap";
+import styles from './sectionContent.module.scss'
+
+const FooterCol = ({ title, link }) => {
+  return (
+    <div className={styles.templateLeft}></div>
+  );
+};
+
+export default FooterCol;
