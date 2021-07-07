7b280f422e6e90edbb6bd72da3da53e8792c00b6
Daniil Zhdanov <yeezysmem@gmail.com>
2021-07-07 20:19:08 +0300

    Update styles

--- src/common/Buttons/OutlinedBtn.jsx
+++ src/common/Buttons/OutlinedBtn.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-
+import './OutlinedBtn.scss'
 import { Button } from 'reactstrap';
-const NewsComponent = ({ description, img, link, article="more.." }) => {
+const OutlinedBtn = ({ description, img, link, article="Подробнее" }) => {
   return (
-    <a href={link}><Button outline color="warning" size="sm">{article}</Button></a>
+    <a href={link}><Button className="myBtn" outline color="danger" size="xl"  style={{width:"95%", borderRadius:0,}}>{article}</Button></a>
   );
 };
 
-export default NewsComponent;
+export default OutlinedBtn;
 // clip-path: polygon(100% 0,100% 28%,99% 30%,99% calc(100% - 12px),calc(99% - 14px) 100%,0 100%,0 0);
