5e4d43c181c9a7244ecd55d1a5e561a9f15b249a
Daniil Zhdanov <43522455+yeezysmem@users.noreply.github.com>
2021-07-27 23:20:32 +0300

    //

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+// import { Suspense, lazy } from "react";
+// import { Switch, Route, Redirect } from "react-router-dom";
+ 
+// const Products = lazy(() => import("./materials/Products/Products"));
+
+// export const getRoutes = () => {
+//   return (
+//     <Suspense>
+//       <Switch>
+//         <Route exact path="/products" component={Products} />
+//       </Switch>
+//     </Suspense>
+//   );
+// };
