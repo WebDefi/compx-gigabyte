1a986aae24e8f7209cb3593b4fb17e585cd8e54d
Daniil Zhdanov <43522455+yeezysmem@users.noreply.github.com>
2021-07-27 23:24:04 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,19 +7,23 @@ import Footer from "./materials/Footer/Footer";
 import Gallery from "./materials/Gallery/Gallery";
 import Products from "./materials/Products/Products";
 import React from 'react';
+import { getRoutes } from "./routes";
+import { withRouter } from "react-router-dom";
+import './static/breakpoints.scss'
 
 function App() {
   return (
     <div className={styles.App}>
       <Header />
+      {/* { getRoutes() }  */}
       <SliderAorus />
       <Devices />
       <News />
       <Gallery />
-      <Products />
+      {/* <Products /> */}
       <Footer />
     </div>
   );
 }
 
-export default App;
+export default withRouter(App);
