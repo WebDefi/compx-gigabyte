5d26def45f123c3230743c735e7ec6c20721c5ad
Daniil Zhdanov <43522455+yeezysmem@users.noreply.github.com>
2021-07-27 23:22:44 +0300

    fixed

--- src/materials/News/components/NewsComponent.jsx
+++ src/materials/News/components/NewsComponent.jsx
@@ -17,4 +17,4 @@ const NewsComponent = ({ description, img }) => {
 };
 
 export default NewsComponent;
-// clip-path: polygon(100% 0,100% 28%,99% 30%,99% calc(100% - 12px),calc(99% - 14px) 100%,0 100%,0 0);
+ 
