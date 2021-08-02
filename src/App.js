2d8ad8e1b2cb4187a1c2d95608550281f7766bae
lamourfouuuu17 <samsonenko_anna_ka-96@gmail.com>
2021-08-02 19:08:41 +0300

    добавила Products на отдельную страницу

--- src/App.js
+++ src/App.js
@@ -8,22 +8,26 @@ import Gallery from "./materials/Gallery/Gallery";
 import Products from "./materials/Products/Products";
 import React from "react";
 import { getRoutes } from "./routes";
-import { withRouter } from "react-router-dom";
+import { withRouter, Route, BrowserRouter, Redirect } from "react-router-dom";
+import HomePage from "./materials/HomePage/HomePage";
 import "./static/breakpoints.scss";
-
 
 function App() {
   return (
-    <div className={styles.App}>
-      <Header />
-      {/* { getRoutes() }  */}
-      <SliderAorus />
-      <Devices />
-      <News />
-      <Gallery />
-      <Products />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className={styles.App}>
+        <Header />
+        {/* { getRoutes() }  */}
+        {/* <SliderAorus />
+        <Devices />
+        <News />
+        <Gallery /> */}
+        <Route path="/products" component={Products} />
+        <Route path="/home" component={HomePage} />
+        <Redirect from="/" to="/home" />
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
