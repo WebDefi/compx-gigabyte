cc6104ab769e9cc89a90dbdd96fc87ffc8d8c2c3
kostiantynn <kostya.code@gmail.com>
2021-11-05 11:56:02 +0200

    fix redirect

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,20 @@
-import {BrowserRouter as Router, Redirect, Route, Switch} from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 import HomePage from "./materials/HomePage/HomePage";
 import ItemsList from "./materials/Products/ItemsList";
-import AdminPanel from "./materials/Admin/Admin"
+import AdminPanel from "./materials/Admin/Admin";
 
-export const Routes =
-    <Router forceRefresh={true}>
-        <Switch>
-            <Route path="/category/:id" component={ItemsList}/>
-            <Route path="/home" component={HomePage}/>
-            <Route path="/admin" component={AdminPanel} />
-            <Redirect from="/" to="/home"/>
-        </Switch>
-    </Router>;
+export const Routes = (
+  <Router forceRefresh={true}>
+    <Switch>
+      <Route path="/category/:id" component={ItemsList} />
+      <Route path="/home" component={HomePage} />
+      <Route path="/admin" component={AdminPanel} />
+      {/* <Redirect from="/" to="/home"/> */}
+    </Switch>
+  </Router>
+);
